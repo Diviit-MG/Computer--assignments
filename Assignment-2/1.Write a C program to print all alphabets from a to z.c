da5c2d68dc076ef6e9300782204a4c4fadcb9e6c
Diviit-MG <96974728+Diviit-MG@users.noreply.github.com>
2022-01-02 14:45:48 +0530

    Update and rename program-1 to 1.Write a C program to print all alphabets from a to z.c

---
+++ Assignment-2/1.Write a C program to print all alphabets from a to z.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    printf("The alphabets:\n");
+    
+    for(c=97;c<=122;c++)
+    {
+        printf("%c\n",c);
+    }
+    
+    return 0;
+}
+
+
